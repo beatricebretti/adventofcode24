587e140dfe67dbc8c32e420ac988ed32793195fc
beatricebretti <bavaldes1@miuandes.cl>
2024-12-01 14:01:09 -0300

    Day 1

---
+++ day1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <unordered_map>
+
+using namespace std;
+
+int similarity(const vector<int>& left, const vector<int>& right) {
+    unordered_map<int, int> counts;
+    for (int num : right) {
+        ++counts[num];
+    }
+
+    int score = 0;
+    for (int num : left) {
+        score += num * counts[num];
+    }
+    return score;
+}
+
+int distance(const vector<int>& left, const vector<int>& right) {
+    vector<int> leftSorted = left;
+    vector<int> rightSorted = right;
+
+    sort(leftSorted.begin(), leftSorted.end());
+    sort(rightSorted.begin(), rightSorted.end());
+
+    int total = 0;
+    for (size_t i = 0; i < leftSorted.size(); ++i) {
+        total += abs(leftSorted[i] - rightSorted[i]);
+    }
+    return total;
+}
+
+int main() {
+    ifstream inputFile("input.txt");
+    vector<int> left, right;
+    int a, b;
+
+    while (inputFile >> a >> b) {
+        left.push_back(a);
+        right.push_back(b);
+    }
+    cout << distance(left, right) << endl;
+    cout << similarity(left, right) << endl;
+    return 0;
+}
