e2a41d7e6c731e84099118fc4454d59a4ed3c554
beatricebretti <bavaldes1@miuandes.cl>
2024-12-01 14:11:34 -0300

    Day 1

--- day1.cpp
+++ day1.cpp
@@ -9,23 +9,16 @@ using namespace std;
 
 int similarity(const vector<int>& left, const vector<int>& right) {
     unordered_map<int, int> counts;
-    for (int num : right) {
-        ++counts[num];
-    }
+    for (int num : right) { ++counts[num]; }
 
     int score = 0;
-    for (int num : left) {
-        score += num * counts[num];
-    }
+    for (int num : left) { score += num * counts[num]; }
     return score;
 }
 
 int distance(const vector<int>& left, const vector<int>& right) {
-    vector<int> leftSorted = left;
-    vector<int> rightSorted = right;
-
-    sort(leftSorted.begin(), leftSorted.end());
-    sort(rightSorted.begin(), rightSorted.end());
+    vector<int> leftSorted = left; vector<int> rightSorted = right;
+    sort(leftSorted.begin(), leftSorted.end()); sort(rightSorted.begin(), rightSorted.end());
 
     int total = 0;
     for (size_t i = 0; i < leftSorted.size(); ++i) {
@@ -40,10 +33,8 @@ int main() {
     int a, b;
 
     while (inputFile >> a >> b) {
-        left.push_back(a);
-        right.push_back(b);
+        left.push_back(a); right.push_back(b);
     }
-    cout << distance(left, right) << endl;
-    cout << similarity(left, right) << endl;
+    cout << distance(left, right) << endl; cout << similarity(left, right) << endl;
     return 0;
 }
