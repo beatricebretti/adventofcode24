f80ae0e1d3531872331bca143e816f57dac7e278
beatricebretti <bavaldes1@miuandes.cl>
2024-12-04 09:29:21 -0300

    Day 4

--- day4.cpp
+++ day4.cpp
@@ -12,7 +12,7 @@ const vector<pair<int, int>> DIR = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1
 
 bool hasXMAS(const vector<string>& grid, int row, int col, pair<int, int> direction);
 int countXMAS(const vector<string>& grid);
-bool hasX(const vector<string>& grid, int row, int col);
+bool hasX(const string& s);
 int countX(const vector<string>& grid);
 
 int main() {
@@ -53,23 +53,17 @@ int countXMAS(const vector<string>& grid) {
 }
 
 // -------------------- part2 -------------------------------------
-bool hasX(const vector<string>& grid, int row, int col) {
-    int rows = grid.size(); int cols = grid[0].size();
-
-    if (row + 2 >= rows || col - 1 < 0 || col + 1 >= cols) { return false; }
-    bool firstRowValid = (grid[row][col - 1] == 'M' && grid[row][col + 1] == 'S') ||
-                         (grid[row][col - 1] == 'S' && grid[row][col + 1] == 'M');
-    bool thirdRowValid = (grid[row + 2][col - 1] == 'M' && grid[row + 2][col + 1] == 'S') ||
-                         (grid[row + 2][col - 1] == 'S' && grid[row + 2][col + 1] == 'M');
-    bool middleRowValid = (grid[row + 1][col] == 'A');
-    return firstRowValid && middleRowValid && thirdRowValid;
-}
+bool hasX(const string& s) { return s == "MAS" || s == "SAM";}
 int countX(const vector<string>& grid) {
     int rows = grid.size(); int cols = grid[0].size();
-    
+
     int count = 0;
-    for (int row = 0; row < rows; ++row) {
-        for (int col = 0; col < cols; ++col) { if (hasX(grid, row, col)) { ++count; } }
+    for (int r = 0; r <= rows - 3; ++r) {
+        for (int c = 0; c <= cols - 3; ++c) {
+            string diag1 = {grid[r][c], grid[r+1][c+1], grid[r+2][c+2]};
+            string diag2 = {grid[r][c+2], grid[r+1][c+1], grid[r+2][c]};
+            if (hasX(diag1) && hasX(diag2)) { ++count; }
+        }
     }
     return count;
 }
